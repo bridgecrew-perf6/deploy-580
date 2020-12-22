669f6b4b947e409875950a94020a6b6f451934b6
che pasha <che.pasha@gmail.com>
2020-12-22 08:06:19 +0300

    Domain has been changed

--- letsencrypt/update/gastronamus.sh
+++ letsencrypt/update/gastronamus.sh
@@ -2,9 +2,9 @@
 
 /usr/bin/certbot certonly \
     --standalone \
-    --email info@gastronamus.doce.solutions \
+    --email info@gastronamus.sacdis.com \
     --renew-by-default \
     --rsa-key-size 4096 \
-    -d gastronamus.doce.solutions \
+    -d gastronamus.sacdis.com \
     --pre-hook 'systemctl stop nginx' \
     --post-hook 'systemctl start nginx'
